144d0a574aed6a3e89db6ba4ab09be3680f35999
jeong03 <hjeong7367@gmail.com>
2025-10-15 17:04:23 +0900

    대거 변경

--- frontend/src/Layout.js
+++ frontend/src/Layout.js
@@ -8,24 +8,37 @@ export default function Layout() {
       
       {/* 🔼 헤더 */}
       <header style={{
-        backgroundColor: '#24292f',
+        backgroundColor: '#F9F4EB',
         color: 'white',
         padding: '16px 48px',
         display: 'flex',
         justifyContent: 'space-between',
         alignItems: 'center'
       }}>
-        <h1 style={{ margin: 0, color: '#DB3A00', fontSize: '38px' }}>InsideOut</h1>
+        <h1 style={{ 
+          margin: 0, 
+          color: '#1D5385', 
+          fontSize: '38px',
+          fontFamily: '"Mungyeong Gamhong Apple", sans-serif'
+          }}>
+            Feelm
+          </h1>
         <nav style={{
           display: 'flex', 
           gap: '32px' 
         }}>
-            <Link to="/" style={{ color: 'white', textDecoration: 'none' }}>홈</Link>
-            <Link to="Mypage" style={{ color: 'white', textDecoration: 'none' }}>마이페이지</Link>
-            <Link to="login" style={{ color: 'white', textDecoration: 'none' }}>로그아웃</Link>
+            <Link to="/" style={{ color: '#1D5385', textDecoration: 'none' }}>홈</Link>
+            <Link to="Mypage" style={{ color: '#1D5385', textDecoration: 'none' }}>마이페이지</Link>
+            <Link to="login" style={{ color: '#1D5385', textDecoration: 'none' }}>로그아웃</Link>
 
         </nav>
       </header>
+
+      <hr style={{
+            border: 'none',
+            borderTop: '1px solid #1D5385',
+            margin: 0
+      }} />
 
       {/* 📄 페이지 내용 */}
       <main style={{ flex: 1, padding: '24px 24px 80px' }}>
